7d582eac70850fa635c55be791e67970dfe50d98
vic-barros <victoria2425laissa@gmail.com>
2026-03-21 13:07:06 -0300

    adicionando metodos POO na classe Pilha

--- src/pilha_projeto/Main.java
+++ src/pilha_projeto/Main.java
@@ -1,5 +1,25 @@
 package pilha_projeto;
 
 public class Main {
+	
+	 public static void main(String[] args) {
+		 Pilha primeiraPilha = new Pilha (30);
+			primeiraPilha.inserir(10);
+			primeiraPilha.inserir(20);
+			primeiraPilha.inserir(30);
+			primeiraPilha.exibir();
+			primeiraPilha.remover();
+			primeiraPilha.exibir();
+			
+			System.out.println("A média dos valores da pilha é: " + primeiraPilha.media());
+			System.out.println("Número Maior: " + primeiraPilha.maior());
+			System.out.println("Número Menor: " + primeiraPilha.menor());
+			
+			primeiraPilha.ordenarCrescente();
+			primeiraPilha.ordenarDecrescente();
+			
 
+	 }
+
+	
 }
--- src/pilha_projeto/Pilha.java
+++ src/pilha_projeto/Pilha.java
@@ -1,5 +1,95 @@
 package pilha_projeto;
 
+
 public class Pilha {
+	
+	int[] elementos;
+	int topo;
+	
+	public Pilha(int tamanho) {
+		elementos = new int [tamanho];
+		topo = -1;
+	}
+	
+	void inserir(int valor) {
+		topo++;
+		elementos[topo] = valor;
+	}
+	
+	void remover() {
+		topo--;
+	}
+	
+	void exibir() {
+		for(int i = topo; i >= 0; i--) {
+			System.out.println("A pilha segue a ordem: " + elementos[i]);
+		}
+	}
+	
+	double media() {
+		double soma = 0;
+		for(int i = 0; i <= topo; i++) {
+			soma = soma + elementos[i];
+		}
+		return soma / (topo +1);
+	}
+	
+	int maior() {
+		int numeroMaior = elementos[0];
+		for(int i = 0; i <= topo; i++) {
+			if(elementos[i] > numeroMaior) {
+				numeroMaior = elementos[i];
+			}
+		}
+		return numeroMaior;
+	}
+	
+	int menor() {
+		int numeroMenor = elementos[0];
+		for(int i = 0; i <= topo; i++) {
+			if(elementos[i] < numeroMenor) {
+				numeroMenor = elementos[i];
+			}
+		}
+		return numeroMenor;
+	}
+	
+	void ordenarCrescente() {
+	    int[] copia = elementos.clone();
+
+	    for (int i = 0; i <= topo; i++) {
+	        for (int j = 0; j < topo; j++) {
+	            if (copia[j] > copia[j + 1]) {
+	                int temp = copia[j];
+	                copia[j] = copia[j + 1];
+	                copia[j + 1] = temp;
+	            }
+	        }
+	    }
+
+	    System.out.println("--- Ordem Crescente ---");
+	    for (int i = 0; i <= topo; i++) {
+	        System.out.println(copia[i]);
+	    }
+	}
+	
+	void ordenarDecrescente() {
+	    int[] copia = elementos.clone();
+
+	    for (int i = 0; i <= topo; i++) {
+	        for (int j = 0; j < topo; j++) {
+	            if (copia[j] < copia[j + 1]) {
+	                int temp = copia[j];
+	                copia[j] = copia[j + 1];
+	                copia[j + 1] = temp;
+	            }
+	        }
+	    }
+
+	    System.out.println("--- Ordem Decrescente ---");
+	    for (int i = 0; i <= topo; i++) {
+	        System.out.println(copia[i]);
+	    }
+	}
 
 }
